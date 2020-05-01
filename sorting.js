339d14101955a4f9d34f527a609e9f7335fa2544
LittleStar21 <leonardian14@gmail.com>
2020-05-02 00:11:55 +0800

    Add vertical bar

--- sorting.js
+++ sorting.js
@@ -1,15 +1,32 @@
 var numbers = [];
 
 function generateNums() {
-    alert("Generate Nums");
-    size = document.getElementById('number').value;
+    var size = document.getElementById('number').value;
     size = parseInt(size);
 
     if (numbers) {
         numbers = [];
     }
     for (let i = 0; i < size; i++) {
-        numbers[i] = Math.floor(Math.random() * 1000) + 1;
+        numbers[i] = Math.floor(Math.random() * 500);
+    }
+    updateDisplay(numbers);
+}
+
+function updateDisplay(numbers) {
+    var x = 10;
+    var width = Math.floor(960 / numbers.length);
+    for (let i = 0; i < numbers.length; i++, x+= width) {
+        var bar = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        var y = 570 - numbers[i];     
+
+        bar.setAttribute("class", "barr");
+        bar.setAttribute("x", x.toString());
+        bar.setAttribute("width", width.toString());
+        bar.setAttribute("y", y.toString());
+        bar.setAttribute("height", numbers[i].toString());
+
+        document.getElementById("groupBar").appendChild(bar);
     }
 }
 
@@ -18,13 +35,13 @@ function linearSort() {
 }
 
 function insertionSort() {
-    alert("Insertion Sort");
+    alert('Insertion Sort');
 }
 
 function bubbleSort() {
-    alert("Bubble Sort");
+    alert('Bubble Sort');
 }
 
 function mergeSort() {
-    alert("Merge Sort");
+    alert('Merge Sort');
 }
