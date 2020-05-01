6015595844c8886f3a651accaf04439c0f36b25c
LittleStar21 <leonardian14@gmail.com>
2020-05-02 01:37:40 +0800

    linearSort done

--- sorting.js
+++ sorting.js
@@ -10,17 +10,23 @@ function generateNums() {
     for (let i = 0; i < size; i++) {
         numbers[i] = Math.floor(Math.random() * 500);
     }
-    updateDisplay(numbers);
+    updateDisplay();
 }
 
-function updateDisplay(numbers) {
-    var x = 10;
-    var width = Math.floor(960 / numbers.length);
+function updateDisplay() {
+    var groupBar = document.getElementById("groupBar");
+    var len = document.getElementById("groupBar").childNodes.length;
+    for (let i = 0; i < len; i++) {
+        groupBar.removeChild(groupBar.childNodes[0]);
+    }
+
+    var x = 50;
+    var width = Math.floor(1000 / numbers.length);
     for (let i = 0; i < numbers.length; i++, x+= width) {
         var bar = document.createElementNS("http://www.w3.org/2000/svg", "rect");
         var y = 570 - numbers[i];     
 
-        bar.setAttribute("class", "barr");
+        bar.setAttribute("class", "numberBar");
         bar.setAttribute("x", x.toString());
         bar.setAttribute("width", width.toString());
         bar.setAttribute("y", y.toString());
@@ -30,12 +36,32 @@ function updateDisplay(numbers) {
     }
 }
 
+function linearSortOneStep(startIndex) {
+    if (startIndex >= numbers.length) {
+        return;
+    }
+
+    var smallestIndex = startIndex;
+
+    for (let i = startIndex; i < numbers.length; i++) {
+        if (numbers[smallestIndex] > numbers[i]) {
+            smallestIndex = i;
+        }
+    }
+    var temp = numbers[smallestIndex];
+    numbers[smallestIndex] = numbers[startIndex];
+    numbers[startIndex] = temp;
+
+    updateDisplay();
+    setTimeout(function(j) {linearSortOneStep(j); console.log(j); }, 100, startIndex + 1);
+}
+
 function linearSort() {
-    alert('Linear Sort');
+    linearSortOneStep(0);
 }
 
 function insertionSort() {
-    alert('Insertion Sort');
+    
 }
 
 function bubbleSort() {
